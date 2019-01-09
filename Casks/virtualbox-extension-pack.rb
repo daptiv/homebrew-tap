0913bb43bd9c8e8bb102091fb4f9c93ed606e3e6
Linus Ruth <linus.ruth@changepoint.com>
2019-01-09 13:36:36 -0800

    feat: cask for installing virtualbox 5.2.22

---
+++ Casks/virtualbox-extension-pack.rb
@@ -0,0 +1,40 @@
+cask 'virtualbox-extension-pack' do
+  version '5.2.22,126460'
+  sha256 '779250666551b2f5426e86c2d21ceb0209b46174536971611025f753535131ef'
+
+  url "https://download.virtualbox.org/virtualbox/#{version.before_comma}/Oracle_VM_VirtualBox_Extension_Pack-#{version.before_comma}-#{version.after_comma}.vbox-extpack"
+  appcast 'https://download.virtualbox.org/virtualbox/LATEST.TXT'
+  name 'Oracle VirtualBox Extension Pack'
+  homepage 'https://www.virtualbox.org/'
+
+  conflicts_with cask: 'virtualbox-extension-pack-beta'
+  depends_on cask: 'virtualbox'
+  container type: :naked
+
+  stage_only true
+
+  postflight do
+    system_command '/usr/local/bin/VBoxManage',
+                   args:  [
+                            'extpack', 'install',
+                            '--replace', "#{staged_path}/Oracle_VM_VirtualBox_Extension_Pack-#{version.before_comma}-#{version.after_comma}.vbox-extpack"
+                          ],
+                   input: 'y',
+                   sudo:  true
+  end
+
+  uninstall_postflight do
+    next unless File.exist?('/usr/local/bin/VBoxManage')
+
+    system_command '/usr/local/bin/VBoxManage',
+                   args: [
+                           'extpack', 'uninstall',
+                           'Oracle VM VirtualBox Extension Pack'
+                         ],
+                   sudo: true
+  end
+
+  caveats do
+    license 'https://www.virtualbox.org/wiki/VirtualBox_PUEL'
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # homebrew-tap
-A Homebrew tap containing alternate versions of casks.
+A [Homebrew tap](https://docs.brew.sh/Taps) containing [alternate versions](https://github.com/Homebrew/homebrew-cask-versions) of [casks](https://caskroom.io/).
+
+# Background
+Casks cannot be versioned.  Running `brew cask install my_app` will install whichever version of `my_app` the cask is designed to install.  You get what you get, which is often the latest available version of an application.
+
+What happens if you need an older version of something?  Or you need a to use a different supported version of something?  Specifying an arbitrary version of an application is not possible.
+
+However, you can create your own repo (which must be named `homebrew-*`) to contain your own set of customized casks, which will install your required versions of things.  Then you can connect to that repo with the `brew tap` command, and prioritize it ahead of the official taps with the `brew tap-pin` command.  That way, when `brew` looks for a cask named `my_app`, it will use the one from your repo.  Version control achieved!
+
+# Usage
+Add a cask to [/Casks](/Casks) which will install `my_app`.
+
+Tap the repo, and prioritize it over the public taps:
+```bash
+brew tap daptiv/homebrew-tap
+brew tap-pin daptiv/homebrew-tap
+```
+
+Then install the cask just like normal:
+```bash
+brew cask install my_app
+```
